5879baee620d2a0a6db0cf9b1aa70e9c22437b8d
Qosmio <datapronix@protonmail.com>
2024-02-06 17:16:06 -0500

    Fix deprecated calls (OpenSSL < 3.0)
    [1/5] Building CXX object CMakeFiles/SpeedTest.dir/MD5Util.cpp.o
    /data/code/SpeedTest/MD5Util.cpp: In static member function ‘static std::string MD5Util::hexDigest(const string&)’:
    /data/code/SpeedTest/MD5Util.cpp:12:13: warning: ‘int MD5_Init(MD5_CTX*)’ is deprecated: Since OpenSSL 3.0 [-Wdeprecated-declarations]
       12 |     MD5_Init(&ctx);
          |     ~~~~~~~~^~~~~~
    In file included from /data/code/SpeedTest/MD5Util.h:14,
                     from /data/code/SpeedTest/MD5Util.cpp:6:
    /usr/include/openssl/md5.h:49:27: note: declared here
       49 | OSSL_DEPRECATEDIN_3_0 int MD5_Init(MD5_CTX *c);
          |                           ^~~~~~~~
    /data/code/SpeedTest/MD5Util.cpp:13:15: warning: ‘int MD5_Update(MD5_CTX*, const void*, size_t)’ is deprecated: Since OpenSSL 3.0 [-Wdeprecated-declarations]
       13 |     MD5_Update(&ctx, str.c_str(), str.size());
          |     ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    In file included from /data/code/SpeedTest/MD5Util.h:14,
                     from /data/code/SpeedTest/MD5Util.cpp:6:
    /usr/include/openssl/md5.h:50:27: note: declared here
       50 | OSSL_DEPRECATEDIN_3_0 int MD5_Update(MD5_CTX *c, const void *data, size_t len);
          |                           ^~~~~~~~~~
    /data/code/SpeedTest/MD5Util.cpp:14:14: warning: ‘int MD5_Final(unsigned char*, MD5_CTX*)’ is deprecated: Since OpenSSL 3.0 [-Wdeprecated-declarations]
       14 |     MD5_Final(digest, &ctx);
          |     ~~~~~~~~~^~~~~~~~~~~~~~
    In file included from /data/code/SpeedTest/MD5Util.h:14,
                     from /data/code/SpeedTest/MD5Util.cpp:6:
    /usr/include/openssl/md5.h:51:27: note: declared here
       51 | OSSL_DEPRECATEDIN_3_0 int MD5_Final(unsigned char *md, MD5_CTX *c);
          |                           ^~~~~~~~~

--- MD5Util.cpp
+++ MD5Util.cpp
@@ -4,19 +4,24 @@
 
 #include <sstream>
 #include "MD5Util.h"
+#include <openssl/evp.h>
+#include <openssl/md5.h>
 
 std::string MD5Util::hexDigest(const std::string &str) {
-    unsigned char digest[MD5_DIGEST_LENGTH];
+  unsigned char digest[MD5_DIGEST_LENGTH];
 
-    MD5_CTX ctx;
-    MD5_Init(&ctx);
-    MD5_Update(&ctx, str.c_str(), str.size());
-    MD5_Final(digest, &ctx);
+  EVP_MD_CTX *mdctx;
+  const EVP_MD *md = EVP_md5();
 
-    char hexDigest[33] = {'\0'};
-    for (int i = 0; i < 16; i++)
-        std::sprintf(&hexDigest[i*2], "%02x", (unsigned int)digest[i]);
+  mdctx = EVP_MD_CTX_new();
+  EVP_DigestInit_ex(mdctx, md, NULL);
+  EVP_DigestUpdate(mdctx, str.c_str(), str.size());
+  EVP_DigestFinal_ex(mdctx, digest, NULL);
+  EVP_MD_CTX_free(mdctx);
 
-    return std::string(hexDigest);
+  char hexDigest[33] = {'\0'};
+  for (int i = 0; i < 16; i++)
+    std::sprintf(&hexDigest[i * 2], "%02x", (unsigned int)digest[i]);
+
+  return std::string(hexDigest);
 }
-
